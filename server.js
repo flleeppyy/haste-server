7277936e3763006514df7cf23c6dd171d7456eb8
flleeppyy <flleeppyy@gmail.com>
2022-08-06 05:18:04 -0700

    fix: uglifyjs not properly throwing error - add check for config.port - add check for log type being empty because eslint wont shut up

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var http = require("http");
-var fs = require("fs");
+// @ts-check
+const http = require("http");
+const fs = require("fs");
 
-var uglify = require("uglify-js");
-var winston = require("winston");
-var connect = require("connect");
-var route = require("connect-route");
-var connect_st = require("st");
-var connect_rate_limit = require("connect-ratelimit");
+const uglify = require("uglify-js");
+const winston = require("winston");
+const connect = require("connect");
+const route = require("connect-route");
+const connect_st = require("st");
+const connect_rate_limit = require("connect-ratelimit");
 
-var DocumentHandler = require("./lib/document_handler");
+const DocumentHandler = require("./lib/document_handler");
 
 // Load the configuration and set some defaults
 const configPath = process.argv.length <= 2 ? "config.js" : process.argv[2];
-const config = require("./config");
-config.port = process.env.PORT || config.port || 7777;
+const config = eval(fs.readFileSync(configPath, "utf8"));
+config.port = process.env.PORT - 0 || config.port || 7777;
 config.host = process.env.HOST || config.host || "localhost";
+
+if (isNaN(config.port)) {
+  throw new Error("Port is not a number");
+}
 
 // Set up the logger
 if (config.logging) {
@@ -24,9 +29,13 @@ if (config.logging) {
     /* was not present */
   }
 
-  var detail, type;
-  for (var i = 0; i < config.logging.length; i++) {
+  let detail, type;
+  for (let i = 0; i < config.logging.length; i++) {
     detail = config.logging[i];
+    // weird error shananigans
+    if (detail.type == null) {
+      detail.type = "console";
+    }
     type = detail.type;
     delete detail.type;
     winston.add(winston.transports[type], detail);
@@ -42,10 +51,10 @@ if (!config.storage.type) {
   config.storage.type = "file";
 }
 
-var Store, preferredStore;
+let Store, preferredStore;
 
 if (process.env.REDISTOGO_URL && config.storage.type === "redis") {
-  var redisClient = require("redis-url").connect(process.env.REDISTOGO_URL);
+  const redisClient = require("redis-url").connect(process.env.REDISTOGO_URL);
   Store = require("./lib/document_stores/redis");
   preferredStore = new Store(config.storage, redisClient);
 } else {
@@ -55,22 +64,29 @@ if (process.env.REDISTOGO_URL && config.storage.type === "redis") {
 
 // Compress the static javascript assets
 if (config.recompressStaticAssets) {
-  var list = fs.readdirSync("./static");
-  for (var j = 0; j < list.length; j++) {
-    var item = list[j];
+  const list = fs.readdirSync("./static");
+  for (let j = 0; j < list.length; j++) {
+    const item = list[j];
     if (item.indexOf(".js") === item.length - 3 && item.indexOf(".min.js") === -1) {
-      var dest = item.substring(0, item.length - 3) + ".min" + item.substring(item.length - 3);
-      var orig_code = fs.readFileSync("./static/" + item, "utf8");
+      const dest = item.substring(0, item.length - 3) + ".min" + item.substring(item.length - 3);
+      const orig_code = fs.readFileSync("./static/" + item, "utf8");
 
-      fs.writeFileSync("./static/" + dest, uglify.minify(orig_code).code, "utf8");
+      const uglifyResult = uglify.minify(orig_code);
+
+      if (uglifyResult.error) {
+        throw uglifyResult.error;
+      }
+
+      console.log(uglifyResult.code);
+      fs.writeFileSync("./static/" + dest, uglifyResult.code, "utf8");
       winston.info("compressed " + item + " into " + dest);
     }
   }
 }
 
 // Send the static documents into the preferred store, skipping expirations
-var path, data;
-for (var name in config.documents) {
+let path, data;
+for (const name in config.documents) {
   path = config.documents[name];
   data = fs.readFileSync(path, "utf8");
   winston.info("loading static document", { name: name, path: path });
@@ -81,7 +97,7 @@ for (var name in config.documents) {
       function (cb) {
         winston.debug("loaded static document", { success: cb });
       },
-      true,
+      true
     );
   } else {
     winston.warn("failed to load static document", { name: name, path: path });
@@ -89,20 +105,20 @@ for (var name in config.documents) {
 }
 
 // Pick up a key generator
-var pwOptions = config.keyGenerator || {};
+const pwOptions = config.keyGenerator || {};
 pwOptions.type = pwOptions.type || "random";
-var gen = require("./lib/key_generators/" + pwOptions.type);
-var keyGenerator = new gen(pwOptions);
+const gen = require("./lib/key_generators/" + pwOptions.type);
+const keyGenerator = new gen(pwOptions);
 
 // Configure the document handler
-var documentHandler = new DocumentHandler({
+const documentHandler = new DocumentHandler({
   store: preferredStore,
   maxLength: config.maxLength,
   keyLength: config.keyLength,
-  keyGenerator: keyGenerator,
+  keyGenerator: keyGenerator
 });
 
-var app = connect();
+const app = connect();
 
 // Rate limit all requests
 if (config.rateLimits) {
@@ -137,7 +153,7 @@ app.use(
     router.head("/documents/:id", function (request, response) {
       return documentHandler.handleGet(request, response, config);
     });
-  }),
+  })
 );
 
 // Otherwise, try to match static files
@@ -146,8 +162,8 @@ app.use(
     path: __dirname + "/static",
     content: { maxAge: config.staticMaxAge },
     passthrough: true,
-    index: false,
-  }),
+    index: false
+  })
 );
 
 // Then we can loop back - and everything else should be a token,
@@ -158,7 +174,7 @@ app.use(
       request.sturl = "/";
       next();
     });
-  }),
+  })
 );
 
 // And match index
@@ -166,8 +182,8 @@ app.use(
   connect_st({
     path: __dirname + "/static",
     content: { maxAge: config.staticMaxAge },
-    index: "index.html",
-  }),
+    index: "index.html"
+  })
 );
 
 http.createServer(app).listen(config.port, config.host);
